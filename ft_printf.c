fa099e13412b0c9caa8a183fac723972eb147cbd
Ayoub Laaoufi <ayoub.laaoufi@hotmail.com>
2024-11-14 06:58:09 +0100

    my second project in 1337

---
+++ ft_printf.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 01:19:15 by aylaaouf          #+#    #+#             */
+/*   Updated: 2024/11/14 06:54:00 by aylaaouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+int ft_printf(const char *s, ...)
+{
+    va_list args;
+    int i;
+    int count;
+
+    va_start(args, s);
+    i = 0;
+    count = 0;
+    while (s[i])
+    {
+        if (s[i] == '%')
+        {
+            i++;
+            if (s[i] == 'c')
+                count += ft_putchar(va_arg(args, int));
+            else if (s[i] == 's')
+                count += ft_putstr(va_arg(args, char *));
+            else if (s[i] == 'p')
+                count += ft_putptr(va_arg(args, void *));
+            else if (s[i] == 'd' || s[i] == 'i')
+                count += ft_putnbr(va_arg(args, int));
+            else if (s[i] == 'u')
+                count += ft_putunsigned(va_arg(args, unsigned int));
+            else if (s[i] == 'x')
+                count += ft_puthex(va_arg(args, unsigned int), 0);
+        }
+        else
+        {
+            count += ft_putchar(s[i]);
+        }
+        i++;
+    }
+    va_end(args);
+    return (count);
+}
---
+++ test.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+
+int main()
+{
+    //printf("%d", 11);
+    ft_printf("%x", 'a');
+}
